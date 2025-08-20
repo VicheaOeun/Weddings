f92299040e4d6081fa5ec2672d976be2ac49044f
CHEA <vichea.oeun@sunfixconsulting.com>
2025-08-20 15:34:23 +0700

    g

--- src/views/Content.vue
+++ src/views/Content.vue
@@ -385,7 +385,7 @@ p {
 /* Normal text */
 p, span, div {
   color: #F5E6CC; /* Champagne */
-  font-size: 15px;
+  font-size: 13px;
   text-shadow: 
   1px 1px 2px rgba(0,0,0,0.5); /* keep only elegant dark shadow */
 }
