9bbf05859a73e8e778dc55f91353abdbab910203
CHEA <vichea.oeun@sunfixconsulting.com>
2025-08-21 22:21:28 +0700

    f

--- vite.config.js
+++ vite.config.js
@@ -8,15 +8,3 @@ export default defineConfig({
     tailwindcss(),
   ],
 })
-
-// tailwind.config.js
-module.exports = {
-  theme: {
-    extend: {
-      fontFamily: {
-        'khmer-muol': ['"Khmer OS Muol"', 'sans-serif'],
-        'khmer-pali': ['"Khmer OS Muol Pali"', 'sans-serif'],
-      },
-    },
-  },
-}
