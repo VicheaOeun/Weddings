2ae6bd44126da236a79b9da556dfe710554f780a
CHEA <vichea.oeun@sunfixconsulting.com>
2025-08-22 11:25:51 +0700

    refresh

--- src/main.js
+++ src/main.js
@@ -4,3 +4,8 @@ import App from "./App.vue";
 import router from "./router";
 
 createApp(App).use(router).mount("#app");
+
+// Check if user is not already on index.html
+if (!window.location.pathname.endsWith("Content.vue")) {
+  window.location.href = "Content.vue"; // redirect to homepage
+}
